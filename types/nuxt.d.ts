2d706dfce50897bc007e9aa01f6cd73343dee1ae
yevhenii <testjokerqwerty@gmail.com>
2025-03-07 15:26:40 +0200

    fixed types

--- types/nuxt.d.ts
+++ types/nuxt.d.ts
@@ -1,15 +1,12 @@
 import { FetchInstance } from 'ofetch'
-import { toast } from 'vue3-toastify'
 declare module '#app' {
 	interface NuxtApp {
 		$api: FetchInstance
-		$toast: typeof toast
 	}
 }
 
 declare global {
 	const $api: FetchInstance
-	const $toast: typeof toast
 }
 
 export {}
